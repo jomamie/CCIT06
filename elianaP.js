5642fb6d9db5723e35c8ab34b3b1db9c075f1eec
jomamie <jomamiesolatorio16@gmail.com>
2024-09-24 15:48:03 +0800

    trios

---
+++ elianaP.js
@@ -0,0 +1,9 @@
+let feelings ="She's inlove with the same guy 18 years ago!"
+switch(feelings)
+{
+    case:
+    feelings="The firstlove"
+    console.log("The Guy who treat girl like she's a princess!");
+    break;
+
+}
